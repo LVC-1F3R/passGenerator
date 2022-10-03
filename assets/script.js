e5ba0523291e105f59ab24be7408cff2abb4e02e
iskry <refocuslucifer@protonmail.com>
2022-10-03 18:58:30 -0500

    added passLength functionality

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,23 @@
 // Assignment code here
+function passLength() {
+  let x = prompt("Pass length? (Must be between 8 to 128 characters.")
+  if (x > 8 && x < 128) {
+    charTypes()
+  } else {
+    generatePassword()
+  }
+  return 
+} 
 
+function charTypes() {
+  
+}
+
+function generatePassword() {
+  let passL = passLength()
+  let passT = charTypes()
+
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -8,7 +26,7 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
+  var x = 2
   passwordText.value = password;
 
 }
